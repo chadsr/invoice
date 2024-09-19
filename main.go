49fb908b89907d5a64c0653ee4206d706ea087f1
Ross <git@ross.ch>
2024-09-19 11:00:12 +0200

    init custom invoice structure

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	_ "embed"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
+	"math"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -20,45 +24,142 @@ var interFont []byte
 var interBoldFont []byte
 
 type Invoice struct {
-	Id    string `json:"id" yaml:"id"`
+	Id       string `json:"id" yaml:"id"`
+	IdPrefix string `json:"idPrefix" yaml:"idPrefix"`
+
+	Website string `json:"website" yaml:"website"`
+	Email   string `json:"email" yaml:"email"`
+
 	Title string `json:"title" yaml:"title"`
 
-	Logo string `json:"logo" yaml:"logo"`
-	From string `json:"from" yaml:"from"`
-	To   string `json:"to" yaml:"to"`
-	Date string `json:"date" yaml:"date"`
-	Due  string `json:"due" yaml:"due"`
+	Logo     string  `json:"logo" yaml:"logo"`
+	LogoSize float64 `json:"logoSize" yaml:"logoSize"`
 
-	Items      []string  `json:"items" yaml:"items"`
-	Quantities []int     `json:"quantities" yaml:"quantities"`
-	Rates      []float64 `json:"rates" yaml:"rates"`
+	From        string            `json:"from" yaml:"from"`
+	FromDetails map[string]string `json:"fromDetails" yaml:"fromDetails"`
+	FromAddress []string          `json:"fromAddress" yaml:"fromAddress"`
+
+	To        string            `json:"to" yaml:"to"`
+	ToDetails map[string]string `json:"toDetails" yaml:"toDetails"`
+	ToAddress []string          `json:"toAddress" yaml:"toAddress"`
+
+	Dates   []time.Time `json:"dates" yaml:"dates"`
+	Date    string      `json:"date" yaml:"date"`
+	Due     string      `json:"due" yaml:"due"`
+	DueDays int         `json:"dueDays" yaml:"dueDays"`
+
+	Items             []string  `json:"items" yaml:"items"`
+	Quantities        []float64 `json:"quantities" yaml:"quantities"`
+	Rates             []float64 `json:"rates" yaml:"rates"`
+	RatesTaxInclusive bool      `json:"ratesTaxInclusive" yaml:"ratesTaxInclusive"`
 
 	Tax      float64 `json:"tax" yaml:"tax"`
+	TaxName  string  `json:"taxName" yaml:"taxName"`
 	Discount float64 `json:"discount" yaml:"discount"`
 	Currency string  `json:"currency" yaml:"currency"`
 
 	Note string `json:"note" yaml:"note"`
 }
 
+type WorklogEntry struct {
+	Date        time.Time
+	Hours       float64
+	Description string
+}
+
+func RoundTo(x, unit float64) float64 {
+	return math.Round(x/unit) * unit
+}
+
+func readWorklogCsv(filePath string) ([]WorklogEntry, error) {
+	entries := make([]WorklogEntry, 0)
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return entries, err
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		return entries, err
+	}
+
+	expHeaders := []string{"Date", "Start", "End", "Worked", "Titles"}
+	for i, record := range records {
+		if i == 0 {
+			for i, header := range record {
+				if header != expHeaders[i] {
+					return entries, fmt.Errorf("unexpected worklog headers")
+				}
+			}
+			continue
+		}
+
+		entryDate, err := time.Parse("2006-01-02", record[0])
+		if err != nil {
+			return entries, err
+		}
+
+		durationValues := strings.Split(record[3], ":")
+		durationHours, err := strconv.Atoi(durationValues[0])
+		if err != nil {
+			return entries, err
+		}
+
+		durationMinutes, err := strconv.Atoi(durationValues[1])
+		if err != nil {
+			return entries, err
+		}
+
+		durationTotal := float64(durationHours) + (float64(durationMinutes) / 60.0)
+
+		entryDescription := record[4]
+
+		entry := WorklogEntry{
+			Date:        entryDate,
+			Hours:       durationTotal,
+			Description: entryDescription,
+		}
+		entries = append(entries, entry)
+	}
+
+	return entries, nil
+}
+
 func DefaultInvoice() Invoice {
 	return Invoice{
-		Id:         time.Now().Format("20060102"),
-		Title:      "INVOICE",
-		Rates:      []float64{25},
-		Quantities: []int{2},
-		Items:      []string{"Paper Cranes"},
-		From:       "Project Folded, Inc.",
-		To:         "Untitled Corporation, Inc.",
-		Date:       time.Now().Format("Jan 02, 2006"),
-		Due:        time.Now().AddDate(0, 0, 14).Format("Jan 02, 2006"),
-		Tax:        0,
-		Discount:   0,
-		Currency:   "USD",
+		Id:                time.Now().Format("200601"),
+		IdPrefix:          "INV",
+		Email:             "some@business.com",
+		Website:           "business.com",
+		Title:             "INVOICE",
+		LogoSize:          100,
+		Rates:             []float64{25},
+		Quantities:        []float64{2},
+		Dates:             []time.Time{time.Now()},
+		Items:             []string{"Paper Cranes"},
+		From:              "Project Folded, Inc.",
+		FromDetails:       map[string]string{},
+		FromAddress:       []string{"1", "Main st", "Newyark", "626112"},
+		To:                "Untitled Corporation, Inc.",
+		ToDetails:         map[string]string{},
+		ToAddress:         []string{"1/56A", "Main st", "Newyark", "626112"},
+		Date:              time.Now().Format("Jan 02, 2006"),
+		Due:               time.Now().AddDate(0, 0, 14).Format("Jan 02, 2006"),
+		DueDays:           14,
+		RatesTaxInclusive: false,
+		Tax:               0,
+		TaxName:           "VAT",
+		Discount:          0,
+		Currency:          "USD",
 	}
 }
 
 var (
-	importPath     string
+	configPath     string
+	worklogPath    string
 	output         string
 	file           = Invoice{}
 	defaultInvoice = DefaultInvoice()
@@ -67,21 +168,35 @@ var (
 func init() {
 	viper.AutomaticEnv()
 
-	generateCmd.Flags().StringVar(&importPath, "import", "", "Imported file (.json/.yaml)")
-	generateCmd.Flags().StringVar(&file.Id, "id", time.Now().Format("20060102"), "ID")
+	generateCmd.Flags().StringVar(&configPath, "config", "", "Config file (.json/.yaml)")
+	generateCmd.Flags().StringVar(&worklogPath, "worklog", "", "Worklog file (.csv)")
+
+	generateCmd.Flags().StringVar(&file.Id, "id", defaultInvoice.Id, "ID")
+	generateCmd.Flags().StringVar(&file.IdPrefix, "id-prefix", defaultInvoice.IdPrefix, "ID Prefix")
 	generateCmd.Flags().StringVar(&file.Title, "title", "INVOICE", "Title")
+	generateCmd.Flags().StringVar(&file.Website, "website", defaultInvoice.Website, "Website")
+	generateCmd.Flags().StringVar(&file.Email, "email", defaultInvoice.Email, "Email address")
 
 	generateCmd.Flags().Float64SliceVarP(&file.Rates, "rate", "r", defaultInvoice.Rates, "Rates")
-	generateCmd.Flags().IntSliceVarP(&file.Quantities, "quantity", "q", defaultInvoice.Quantities, "Quantities")
+	generateCmd.Flags().Float64SliceVarP(&file.Quantities, "quantity", "q", defaultInvoice.Quantities, "Quantities")
 	generateCmd.Flags().StringSliceVarP(&file.Items, "item", "i", defaultInvoice.Items, "Items")
 
 	generateCmd.Flags().StringVarP(&file.Logo, "logo", "l", defaultInvoice.Logo, "Company logo")
+	generateCmd.Flags().Float64Var(&file.LogoSize, "logo-size", defaultInvoice.LogoSize, "Logo size")
+
 	generateCmd.Flags().StringVarP(&file.From, "from", "f", defaultInvoice.From, "Issuing company")
+	generateCmd.Flags().StringSliceVar(&file.FromAddress, "from-address", defaultInvoice.FromAddress, "Issuing company address")
+
 	generateCmd.Flags().StringVarP(&file.To, "to", "t", defaultInvoice.To, "Recipient company")
+	generateCmd.Flags().StringSliceVarP(&file.ToAddress, "to-address", "a", defaultInvoice.ToAddress, "Receiving company address")
+
 	generateCmd.Flags().StringVar(&file.Date, "date", defaultInvoice.Date, "Date")
 	generateCmd.Flags().StringVar(&file.Due, "due", defaultInvoice.Due, "Payment due date")
+	generateCmd.Flags().IntVar(&file.DueDays, "due-days", defaultInvoice.DueDays, "Payment due days after generation date")
 
+	generateCmd.Flags().BoolVar(&file.RatesTaxInclusive, "rates-tax-inclusive", defaultInvoice.RatesTaxInclusive, "Rates tax inclusive")
 	generateCmd.Flags().Float64Var(&file.Tax, "tax", defaultInvoice.Tax, "Tax")
+	generateCmd.Flags().StringVar(&file.TaxName, "tax-name", defaultInvoice.TaxName, "Tax Name")
 	generateCmd.Flags().Float64VarP(&file.Discount, "discount", "d", defaultInvoice.Discount, "Discount")
 	generateCmd.Flags().StringVarP(&file.Currency, "currency", "c", defaultInvoice.Currency, "Currency")
 
@@ -103,20 +218,47 @@ var generateCmd = &cobra.Command{
 	Long:  `Generate an invoice`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		if importPath != "" {
-			err := importData(importPath, &file, cmd.Flags())
+		if configPath != "" {
+			err := importData(configPath, &file, cmd.Flags())
 			if err != nil {
 				return err
 			}
 		}
 
+		worklogEntries, err := readWorklogCsv(worklogPath)
+		if err != nil {
+			return err
+		}
+
+		if len(worklogEntries) > 0 {
+			file.Items = make([]string, len(worklogEntries))
+			file.Quantities = make([]float64, len(worklogEntries))
+			file.Dates = make([]time.Time, len(worklogEntries))
+
+			for i, entry := range worklogEntries {
+				file.Items[i] = entry.Description
+				file.Quantities[i] = entry.Hours
+				file.Dates[i] = entry.Date
+			}
+		}
+
+		file.Id = fmt.Sprintf("%s%s", file.IdPrefix, file.Id)
+
 		pdf := gopdf.GoPdf{}
 		pdf.Start(gopdf.Config{
 			PageSize: *gopdf.PageSizeA4,
+			Protection: gopdf.PDFProtectionConfig{
+				UseProtection: true,
+				Permissions:   gopdf.PermissionsPrint,
+			},
 		})
 		pdf.SetMargins(40, 40, 40, 40)
 		pdf.AddPage()
-		err := pdf.AddTTFFontData("Inter", interFont)
+
+		pageWidth := gopdf.PageSizeA4.W - pdf.MarginLeft() - pdf.MarginRight()
+		// pageHeight := gopdf.PageSizeA4.H - pdf.MarginTop() - pdf.MarginBottom()
+
+		err = pdf.AddTTFFontData("Inter", interFont)
 		if err != nil {
 			return err
 		}
@@ -126,34 +268,81 @@ var generateCmd = &cobra.Command{
 			return err
 		}
 
-		writeLogo(&pdf, file.Logo, file.From)
-		writeTitle(&pdf, file.Title, file.Id, file.Date)
-		writeBillTo(&pdf, file.To)
-		writeHeaderRow(&pdf)
-		subtotal := 0.0
-		for i := range file.Items {
-			q := 1
-			if len(file.Quantities) > i {
-				q = file.Quantities[i]
-			}
-
-			r := 0.0
-			if len(file.Rates) > i {
-				r = file.Rates[i]
-			}
-
-			writeRow(&pdf, file.Items[i], q, r)
-			subtotal += float64(q) * r
+		err = writeTitle(&pdf, file.Title, file.Id, file.Date)
+		if err != nil {
+			return err
 		}
+
+		err = writeLogo(&pdf, file.Logo, file.LogoSize)
+		if err != nil {
+			return err
+		}
+
+		pdf.Br(24)
+
+		currentX := pdf.GetX()
+		currentY := pdf.GetY()
+
+		err = writeCompanyInfo(&pdf, currentX, currentY, file.To, file.ToDetails, file.ToAddress, gopdf.CellOption{})
+		if err != nil {
+			return err
+		}
+
+		err = writeCompanyInfo(&pdf, pageWidth-60, currentY, file.From, file.FromDetails, file.FromAddress, gopdf.CellOption{Align: gopdf.Right})
+		if err != nil {
+			return err
+		}
+
+		pdf.Br(18)
+
+		totalCost, totalHours, err := writeItems(&pdf, pageWidth, currencySymbols[file.Currency], file.Items, file.Quantities, file.Rates, file.Dates)
+		if err != nil {
+			return err
+		}
+
+		pdf.Br(32)
+
+		xPos := pdf.GetX()
+		yPos := pdf.GetY()
+
 		if file.Note != "" {
-			writeNotes(&pdf, file.Note)
+			err = writeNotes(&pdf, file.Note)
+			if err != nil {
+				return err
+			}
 		}
-		writeTotals(&pdf, subtotal, subtotal*file.Tax, subtotal*file.Discount)
+
+		pdf.SetXY(xPos, yPos)
+
+		err = writeTotalHours(&pdf, totalHours)
+		if err != nil {
+			return err
+		}
+
+		err = writeTotals(&pdf, totalCost, totalCost*file.Discount, file.Tax, file.RatesTaxInclusive, file.TaxName)
+		if err != nil {
+			return err
+		}
+
 		if file.Due != "" {
-			writeDueDate(&pdf, file.Due)
+			err = writeDueDate(&pdf, file.Due)
+			if err != nil {
+				return err
+			}
+		} else if file.DueDays > 0 {
+			dueDate := time.Now().AddDate(0, 0, file.DueDays).Format("Jan 02, 2006")
+			err = writeDueDate(&pdf, dueDate)
+			if err != nil {
+				return err
+			}
 		}
-		writeFooter(&pdf, file.Id)
-		output = strings.TrimSuffix(output, ".pdf") + ".pdf"
+
+		err = writeFooter(&pdf, file.Id)
+		if err != nil {
+			return err
+		}
+
+		output = fmt.Sprintf("%s_%s.pdf", strings.TrimSuffix(output, ".pdf"), file.Id)
 		err = pdf.WritePdf(output)
 		if err != nil {
 			return err
--- pdf.go
+++ pdf.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"image"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/signintech/gopdf"
 )
@@ -20,190 +22,429 @@ const (
 	subtotalLabel = "Subtotal"
 	discountLabel = "Discount"
 	taxLabel      = "Tax"
-	totalLabel    = "Total"
+	totalLabel    = "Total Due"
 )
 
-func writeLogo(pdf *gopdf.GoPdf, logo string, from string) {
-	if logo != "" {
-		width, height := getImageDimension(logo)
-		scaledWidth := 100.0
-		scaledHeight := float64(height) * scaledWidth / float64(width)
-		_ = pdf.Image(logo, pdf.GetX(), pdf.GetY(), &gopdf.Rect{W: scaledWidth, H: scaledHeight})
-		pdf.Br(scaledHeight + 24)
+func writeAddress(pdf *gopdf.GoPdf, locX float64, rect *gopdf.Rect, fromAddress []string, cellOption *gopdf.CellOption) error {
+	for _, v := range fromAddress {
+		pdf.SetX(locX)
+		err := pdf.CellWithOption(rect, v, *cellOption)
+		if err != nil {
+			return err
+		}
+
+		pdf.Br(18)
 	}
-	pdf.SetTextColor(55, 55, 55)
 
-	formattedFrom := strings.ReplaceAll(from, `\n`, "\n")
-	fromLines := strings.Split(formattedFrom, "\n")
+	return nil
+}
 
-	for i := 0; i < len(fromLines); i++ {
+func writeDetails(pdf *gopdf.GoPdf, locX float64, rect *gopdf.Rect, details map[string]string, cellOption *gopdf.CellOption) error {
+	keys := make([]string, 0, len(details))
+	for k := range details {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		pdf.SetX(locX)
+		err := pdf.CellWithOption(rect, fmt.Sprintf("%s: %s", k, details[k]), *cellOption)
+		if err != nil {
+			return err
+		}
+
+		pdf.Br(18)
+	}
+
+	return nil
+}
+
+func writeLogo(pdf *gopdf.GoPdf, logo string, logoSize float64) error {
+	origX := pdf.GetX()
+	origY := pdf.GetY()
+	defer pdf.SetXY(origX, origY)
+
+	if logo != "" {
+		pdf.SetXY(gopdf.PageSizeA4.W-logoSize-pdf.MarginRight(), 0+pdf.MarginTop())
+		width, height, err := getImageDimension(logo)
+		if err != nil {
+			return err
+		}
+
+		scaledWidth := logoSize
+		scaledHeight := float64(height) * scaledWidth / float64(width)
+
+		err = pdf.Image(logo, pdf.GetX(), pdf.GetY(), &gopdf.Rect{W: scaledWidth, H: scaledHeight})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func writeTitle(pdf *gopdf.GoPdf, title, id, date string) error {
+	err := pdf.SetFont("Inter-Bold", "", 24)
+	if err != nil {
+		return err
+	}
+
+	pdf.SetTextColor(0, 0, 0)
+	err = pdf.Cell(nil, title)
+	if err != nil {
+		return err
+	}
+
+	pdf.Br(36)
+	err = pdf.SetFont("Inter", "", 12)
+	if err != nil {
+		return err
+	}
+
+	pdf.SetTextColor(100, 100, 100)
+	err = pdf.Cell(nil, "#")
+	if err != nil {
+		return err
+	}
+
+	err = pdf.Cell(nil, id)
+	if err != nil {
+		return err
+	}
+
+	pdf.SetTextColor(150, 150, 150)
+	err = pdf.Cell(nil, "  ·  ")
+	if err != nil {
+		return err
+	}
+
+	pdf.SetTextColor(100, 100, 100)
+
+	err = pdf.Cell(nil, date)
+	if err != nil {
+		return err
+	}
+	pdf.Br(12)
+	return nil
+}
+
+func writeCompanyInfo(pdf *gopdf.GoPdf, locX float64, locY float64, name string, details map[string]string, address []string, cellOption gopdf.CellOption) error {
+	pdf.SetXY(locX, locY)
+	pdf.SetTextColor(75, 75, 75)
+
+	formattedName := strings.ReplaceAll(name, `\n`, "\n")
+	nameLines := strings.Split(formattedName, "\n")
+
+	rectangle := gopdf.Rect{
+		W: 100.0,
+		H: 45.0,
+	}
+
+	for i := 0; i < len(nameLines); i++ {
 		if i == 0 {
-			_ = pdf.SetFont("Inter", "", 12)
-			_ = pdf.Cell(nil, fromLines[i])
+			err := pdf.SetFont("Inter-Bold", "", 12)
+			if err != nil {
+				return err
+			}
+
+			err = pdf.CellWithOption(&rectangle, nameLines[i], cellOption)
+			if err != nil {
+				return err
+			}
+
 			pdf.Br(18)
 		} else {
-			_ = pdf.SetFont("Inter", "", 10)
-			_ = pdf.Cell(nil, fromLines[i])
-			pdf.Br(15)
+			err := pdf.SetFont("Inter", "", 10)
+			if err != nil {
+				return err
+			}
+
+			err = pdf.CellWithOption(&rectangle, nameLines[i], cellOption)
+			if err != nil {
+				return err
+			}
+
+			pdf.Br(14)
 		}
+		pdf.SetX(locX)
 	}
-	pdf.Br(21)
-	pdf.SetStrokeColor(225, 225, 225)
-	pdf.Line(pdf.GetX(), pdf.GetY(), 260, pdf.GetY())
-	pdf.Br(36)
+
+	err := pdf.SetFont("Inter", "", 10)
+	if err != nil {
+		return err
+	}
+
+	err = writeDetails(pdf, locX, &rectangle, details, &cellOption)
+	if err != nil {
+		return err
+	}
+
+	err = writeAddress(pdf, locX, &rectangle, address, &cellOption)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func writeTitle(pdf *gopdf.GoPdf, title, id, date string) {
-	_ = pdf.SetFont("Inter-Bold", "", 24)
-	pdf.SetTextColor(0, 0, 0)
-	_ = pdf.Cell(nil, title)
-	pdf.Br(36)
-	_ = pdf.SetFont("Inter", "", 12)
-	pdf.SetTextColor(100, 100, 100)
-	_ = pdf.Cell(nil, "#")
-	_ = pdf.Cell(nil, id)
-	pdf.SetTextColor(150, 150, 150)
-	_ = pdf.Cell(nil, "  ·  ")
-	pdf.SetTextColor(100, 100, 100)
-	_ = pdf.Cell(nil, date)
-	pdf.Br(48)
-}
+func writeTotalHours(pdf *gopdf.GoPdf, totalHours float64) error {
+	err := pdf.SetFont("Inter", "", 9)
+	if err != nil {
+		return err
+	}
 
-func writeDueDate(pdf *gopdf.GoPdf, due string) {
-	_ = pdf.SetFont("Inter", "", 9)
 	pdf.SetTextColor(75, 75, 75)
 	pdf.SetX(rateColumnOffset)
-	_ = pdf.Cell(nil, "Due Date")
+	err = pdf.Cell(nil, "Total Hours")
+	if err != nil {
+		return err
+	}
+
 	pdf.SetTextColor(0, 0, 0)
-	_ = pdf.SetFontSize(11)
+	err = pdf.SetFontSize(11)
+	if err != nil {
+		return err
+	}
+
 	pdf.SetX(amountColumnOffset - 15)
-	_ = pdf.Cell(nil, due)
-	pdf.Br(12)
-}
-
-func writeBillTo(pdf *gopdf.GoPdf, to string) {
-	pdf.SetTextColor(75, 75, 75)
-	_ = pdf.SetFont("Inter", "", 9)
-	_ = pdf.Cell(nil, "BILL TO")
-	pdf.Br(18)
-	pdf.SetTextColor(75, 75, 75)
-
-	formattedTo := strings.ReplaceAll(to, `\n`, "\n")
-	toLines := strings.Split(formattedTo, "\n")
-
-	for i := 0; i < len(toLines); i++ {
-		if i == 0 {
-			_ = pdf.SetFont("Inter", "", 15)
-			_ = pdf.Cell(nil, toLines[i])
-			pdf.Br(20)
-		} else {
-			_ = pdf.SetFont("Inter", "", 10)
-			_ = pdf.Cell(nil, toLines[i])
-			pdf.Br(15)
-		}
+	err = pdf.Cell(nil, fmt.Sprintf("%.2f", totalHours))
+	if err != nil {
+		return err
 	}
-	pdf.Br(64)
-}
 
-func writeHeaderRow(pdf *gopdf.GoPdf) {
-	_ = pdf.SetFont("Inter", "", 9)
-	pdf.SetTextColor(55, 55, 55)
-	_ = pdf.Cell(nil, "ITEM")
-	pdf.SetX(quantityColumnOffset)
-	_ = pdf.Cell(nil, "QTY")
-	pdf.SetX(rateColumnOffset)
-	_ = pdf.Cell(nil, "RATE")
-	pdf.SetX(amountColumnOffset)
-	_ = pdf.Cell(nil, "AMOUNT")
 	pdf.Br(24)
+	return nil
 }
 
-func writeNotes(pdf *gopdf.GoPdf, notes string) {
-	pdf.SetY(600)
+func writeDueDate(pdf *gopdf.GoPdf, due string) error {
+	err := pdf.SetFont("Inter", "", 9)
+	if err != nil {
+		return err
+	}
 
-	_ = pdf.SetFont("Inter", "", 9)
+	pdf.SetTextColor(75, 75, 75)
+	pdf.SetX(rateColumnOffset)
+	err = pdf.Cell(nil, "Due Date")
+	if err != nil {
+		return err
+	}
+
+	pdf.SetTextColor(0, 0, 0)
+	err = pdf.SetFontSize(11)
+	if err != nil {
+		return err
+	}
+
+	pdf.SetX(amountColumnOffset - 15)
+	err = pdf.Cell(nil, due)
+	if err != nil {
+		return err
+	}
+
+	pdf.Br(24)
+	return nil
+}
+
+func writeNotes(pdf *gopdf.GoPdf, notes string) error {
+	err := pdf.SetFont("Inter", "", 9)
+	if err != nil {
+		return err
+	}
+
 	pdf.SetTextColor(55, 55, 55)
-	_ = pdf.Cell(nil, "NOTES")
+	err = pdf.Cell(nil, "NOTES")
+	if err != nil {
+		return err
+	}
+
 	pdf.Br(18)
-	_ = pdf.SetFont("Inter", "", 9)
+	err = pdf.SetFont("Inter", "", 9)
+	if err != nil {
+		return err
+	}
+
 	pdf.SetTextColor(0, 0, 0)
 
 	formattedNotes := strings.ReplaceAll(notes, `\n`, "\n")
 	notesLines := strings.Split(formattedNotes, "\n")
 
 	for i := 0; i < len(notesLines); i++ {
-		_ = pdf.Cell(nil, notesLines[i])
+		err = pdf.Cell(nil, notesLines[i])
+		if err != nil {
+			return err
+		}
 		pdf.Br(15)
 	}
 
 	pdf.Br(48)
+	return nil
 }
-func writeFooter(pdf *gopdf.GoPdf, id string) {
+
+func writeFooter(pdf *gopdf.GoPdf, id string) error {
 	pdf.SetY(800)
 
-	_ = pdf.SetFont("Inter", "", 10)
+	err := pdf.SetFont("Inter", "", 10)
+	if err != nil {
+		return err
+	}
+
 	pdf.SetTextColor(55, 55, 55)
-	_ = pdf.Cell(nil, id)
+	err = pdf.Cell(nil, id)
+	if err != nil {
+		return err
+	}
+
 	pdf.SetStrokeColor(225, 225, 225)
 	pdf.Line(pdf.GetX()+10, pdf.GetY()+6, 550, pdf.GetY()+6)
 	pdf.Br(48)
+	return nil
 }
 
-func writeRow(pdf *gopdf.GoPdf, item string, quantity int, rate float64) {
-	_ = pdf.SetFont("Inter", "", 11)
-	pdf.SetTextColor(0, 0, 0)
+func writeItems(pdf *gopdf.GoPdf, width float64, currencySymbol string, items []string, quantities []float64, rates []float64, dates []time.Time) (totalCost float64, totalHours float64, err error) {
+	itemsTable := pdf.NewTableLayout(pdf.GetX(), pdf.GetY(), 24, len(items))
+	itemsTable.SetTableStyle(gopdf.CellStyle{
+		BorderStyle: gopdf.BorderStyle{
+			RGBColor: gopdf.RGBColor{R: 75, G: 75, B: 75},
+			Top:      true,
+			Bottom:   true,
+		},
+	})
+	itemsTable.SetHeaderStyle(gopdf.CellStyle{
+		BorderStyle: gopdf.BorderStyle{},
+		Font:        "Inter-Bold",
+		TextColor:   gopdf.RGBColor{R: 75, G: 75, B: 75},
+		FontSize:    11,
+	})
 
-	total := float64(quantity) * rate
-	amount := strconv.FormatFloat(total, 'f', 2, 64)
+	itemsTable.SetCellStyle(gopdf.CellStyle{
+		TextColor:   gopdf.RGBColor{R: 55, G: 55, B: 55},
+		BorderStyle: gopdf.BorderStyle{},
+		Font:        "Inter",
+		FontSize:    8,
+	})
 
-	_ = pdf.Cell(nil, item)
-	pdf.SetX(quantityColumnOffset)
-	_ = pdf.Cell(nil, strconv.Itoa(quantity))
-	pdf.SetX(rateColumnOffset)
-	_ = pdf.Cell(nil, currencySymbols[file.Currency]+strconv.FormatFloat(rate, 'f', 2, 64))
-	pdf.SetX(amountColumnOffset)
-	_ = pdf.Cell(nil, currencySymbols[file.Currency]+amount)
-	pdf.Br(24)
-}
+	itemsTable.AddColumn("DATE", (width / 8), "center")
+	itemsTable.AddColumn("DESCRIPTION", (width / 2), "center")
+	itemsTable.AddColumn("HOURS", (width / 8), "center")
+	itemsTable.AddColumn("RATE", (width / 8), "center")
+	itemsTable.AddColumn("AMOUNT", (width / 8), "center")
 
-func writeTotals(pdf *gopdf.GoPdf, subtotal float64, tax float64, discount float64) {
-	pdf.SetY(600)
+	for i := range items {
+		quantity := 1.0
+		if len(quantities) > i {
+			quantity = quantities[i]
+		}
 
-	writeTotal(pdf, subtotalLabel, subtotal)
-	if tax > 0 {
-		writeTotal(pdf, taxLabel, tax)
+		rate := rates[0]
+		if len(rates) > i {
+			rate = rates[i]
+		}
+
+		total := float64(quantity) * rate
+		itemsTable.AddRow([]string{
+			dates[i].Format("02-01-2006"),
+			items[i],
+			strconv.FormatFloat(quantity, 'f', 2, 64),
+			fmt.Sprintf("%s%.2f", currencySymbol, rate),
+			fmt.Sprintf("%s%.2f", currencySymbol, total),
+		})
+
+		totalCost += float64(quantity) * rate
+		totalHours += quantity
 	}
-	if discount > 0 {
-		writeTotal(pdf, discountLabel, discount)
+
+	err = itemsTable.DrawTable()
+	if err != nil {
+		return
 	}
-	writeTotal(pdf, totalLabel, subtotal+tax-discount)
+
+	return
 }
 
-func writeTotal(pdf *gopdf.GoPdf, label string, total float64) {
-	_ = pdf.SetFont("Inter", "", 9)
+func writeTotal(pdf *gopdf.GoPdf, label string, total float64) error {
+	err := pdf.SetFont("Inter", "", 9)
+	if err != nil {
+		return err
+	}
+
 	pdf.SetTextColor(75, 75, 75)
 	pdf.SetX(rateColumnOffset)
-	_ = pdf.Cell(nil, label)
+	err = pdf.Cell(nil, label)
+	if err != nil {
+		return err
+	}
+
 	pdf.SetTextColor(0, 0, 0)
-	_ = pdf.SetFontSize(12)
+	err = pdf.SetFontSize(12)
+	if err != nil {
+		return err
+	}
+
 	pdf.SetX(amountColumnOffset - 15)
 	if label == totalLabel {
-		_ = pdf.SetFont("Inter-Bold", "", 11.5)
+		err = pdf.SetFont("Inter-Bold", "", 11.5)
+		if err != nil {
+			return err
+		}
 	}
-	_ = pdf.Cell(nil, currencySymbols[file.Currency]+strconv.FormatFloat(total, 'f', 2, 64))
+	err = pdf.Cell(nil, currencySymbols[file.Currency]+strconv.FormatFloat(total, 'f', 2, 64))
+	if err != nil {
+		return err
+	}
+
 	pdf.Br(24)
+	return nil
 }
 
-func getImageDimension(imagePath string) (int, int) {
+func writeTotals(pdf *gopdf.GoPdf, subtotal float64, discount float64, taxRate float64, taxInclusive bool, taxName string) error {
+	if taxInclusive {
+		err := writeTotal(pdf, subtotalLabel, subtotal-(subtotal*taxRate))
+		if err != nil {
+			return err
+		}
+	} else {
+		err := writeTotal(pdf, subtotalLabel, subtotal)
+		if err != nil {
+			return err
+		}
+	}
+
+	if taxRate > 0 {
+		err := writeTotal(pdf, fmt.Sprintf("%s %.0f%%", taxName, taxRate*100), subtotal*taxRate)
+		if err != nil {
+			return err
+		}
+	}
+	if discount > 0 {
+		err := writeTotal(pdf, discountLabel, discount)
+		if err != nil {
+			return err
+		}
+	}
+
+	total := subtotal - discount
+	if !taxInclusive && taxRate > 0 {
+		total += (subtotal * taxRate)
+	}
+
+	err := writeTotal(pdf, totalLabel, total)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getImageDimension(imagePath string) (int, int, error) {
 	file, err := os.Open(imagePath)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return 0, 0, err
 	}
 	defer file.Close()
 
 	image, _, err := image.DecodeConfig(file)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %v\n", imagePath, err)
+		return 0, 0, err
 	}
-	return image.Width, image.Height
+	return image.Width, image.Height, nil
 }
